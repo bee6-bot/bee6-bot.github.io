508fba82719ad4b5e768e2484afdc6ee54acded9
BeauTheBeau <beauthebeau@proton.me>
2023-10-13 23:18:55 +0100

    Add last page tracking functionality to bottom bar links
    This update modifies the bottom bar links to include a 'lastPage' parameter within the URL to denote the previously active page. On load, the script now checks this parameter, comparing it to the `href` of each link, and applies the 'active' class to any match. This allows the website to keep the state of the previously viewed page even after refreshing or revisiting the site. Delayed code execution (set @ 250ms) now takes place after updates to link hrefs to ensure accurate tracking.

--- js/main.js
+++ js/main.js
@@ -12,11 +12,21 @@ window.addEventListener('resize', () => {
 
 // For each a in the .bottom__bar
 const bottomBarLinks = document.querySelectorAll('.bottom__bar a');
-bottomBarLinks.forEach((link) => {
-    // 250ms delay
-    setTimeout(() => {
 
+// FInd the link that matches the current page and add the active class
+const lastPage = window.location.search.split('?lastPage=')[1];
+if (lastPage) {
+    bottomBarLinks.forEach((link) => { if (link.href.split('?')[0] === lastPage) link.classList.add('active'); });
+}
+
+bottomBarLinks.forEach((link) => {
     const currentLocation = window.location.href.split('?')[0];
-    if (link.href === currentLocation) link.classList.add('active');
+    link.href = `${link.href}?lastPage=${currentLocation}`;
+
+    setTimeout(() => {
+        if (link.href.split('?')[0] === currentLocation) {
+            bottomBarLinks.forEach((link) => link.classList.remove('active'));
+            link.classList.add('active');
+        }
     }, 250);
 });
